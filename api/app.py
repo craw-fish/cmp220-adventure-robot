c39c6d4abaf56ef7ff49561095672b389ba5b439
craw-fish <69093019+craw-fish@users.noreply.github.com>
2025-03-26 15:15:58 -0400

    api: add sqlalchemy models
    columns and constraints match existing sqlite database

--- api/app.py
+++ api/app.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask, request
-# from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
-import os
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -14,6 +15,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # initialize sqlalchemy
 db = SQLAlchemy()
 db.init_app(app)
+
+class Robot(db.Model):
+    __tablename__ = 'robots'
+    # columns
+    robot_id: Mapped[int] = mapped_column(primary_key=True)
+    robot_name: Mapped[str]
+    # one-to-many relationship with snapshots
+    snapshots = db.relationship('Snapshot', back_populates='robot')
+
+class Snapshot(db.Model):
+    __tablename__ = 'snapshots'
+    # columns
+    snapshot_id: Mapped[int] = mapped_column(primary_key=True)
+    robot_id: Mapped[int] = mapped_column(ForeignKey('robots.robot_id'), nullable=False)
+    timestamp: Mapped[str]
+    photo_path: Mapped[str]
+    description: Mapped[str]
+    # many-to-one relationship with robots
+    robot = db.relationship('Robot', back_populates='snapshots')
 
 # test connection at http://localhost:5001/
 # code from https://python-adv-web-apps.readthedocs.io/en/latest/flask_db1.html
