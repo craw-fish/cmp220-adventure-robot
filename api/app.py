a6007edc487daabcd46243a4bff13d08ee6dddbc
craw-fish <69093019+craw-fish@users.noreply.github.com>
2025-03-27 13:39:26 -0400

    api: add query test, refactor connection test

--- api/app.py
+++ api/app.py
@@ -9,7 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # initialize flask app
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(basedir, 'database', 'adventure_log.db')}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, 'database', 'adventure_log.db')}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # initialize sqlalchemy
@@ -35,13 +35,30 @@ class Snapshot(db.Model):
     # many-to-one relationship with robots
     robot = db.relationship('Robot', back_populates='snapshots')
 
-# test connection at http://localhost:5001/
-# code from https://python-adv-web-apps.readthedocs.io/en/latest/flask_db1.html
-@app.route('/')
-def testdb():
+
+# DEBUG
+def test_query():
+    # add an entry to robot and snapshot tables
+    db.session.add(Robot(robot_name='Justin'))
+    db.session.add(Snapshot(robot_id=1, timestamp='2025-27-03 13:22:00'))
+    robots = db.session.execute(db.select(Robot)).scalars()
+    # list snapshots for each robot
+    for robot in robots:
+        print(f'ROBOT {robot.robot_id} ({robot.robot_name})')
+        for snapshot in robot.snapshots:
+            print(f'- Snapshot {snapshot.snapshot_id} taken {snapshot.timestamp}')
+    db.session.rollback()
+
+with app.app_context():        
+    test_query()
+
+# test connection at http://localhost:5001/test_db
+# code adapted from https://python-adv-web-apps.readthedocs.io/en/latest/flask_db1.html
+@app.route('/test_db')
+def test_db():
     try:
         db.session.query(text('1')).from_statement(text('SELECT 1')).all()
-        return '<h1>It works.</h1>'
+        return '<h1>Connection successful.</h1>'
     except Exception as e:
         # e holds description of the error
         error_text = "<p>The error:<br>" + str(e) + "</p>"
