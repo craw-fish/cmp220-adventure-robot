e14dedf81676e71da7fcf4feaa1a47497818a735
craw-fish <69093019+craw-fish@users.noreply.github.com>
2025-04-14 21:40:30 -0400

    api: update config.py for web server
    Use absolute path for upload folder. Disable debug mode.

--- api/config.py
+++ api/config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config():
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, 'database', 'adventure_log.db')}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'uploads/snapshots'
-    DEBUG = True
+    UPLOAD_FOLDER = os.path.join(basedir, 'uploads', 'snapshots')
+    DEBUG = False
